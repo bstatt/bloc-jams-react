459896ecaf446a25091dbcacde8f8679e87eb684
Britany Statt <bstatt2@gmail.com>
2018-05-20 16:29:30 -0700

    album assignment completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -32,6 +32,13 @@ class Album extends Component {
             <col id="song-duration-column" />
           </colgroup>
           <tbody>
+          {this.state.album.songs.map( (song, index) =>
+            <tr key={index}>
+             <td>{index + 1}</td>
+             <td>{this.state.album.songs[index].title}</td>
+             <td>{this.state.album.songs[index].duration}</td>
+            </tr>
+          )}
           </tbody>
         </table>
       </section>
